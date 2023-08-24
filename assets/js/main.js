5763a44fec7afcb94059b6775ef64ebe8d330d6d
rhsrabon <141764590+rhsrabon@users.noreply.github.com>
2023-08-24 01:11:51 -0700

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:4000,
+    smartSpeed:750,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:false
+        },
+        600:{
+            items:2,
+            nav:false
+        },
+        1000:{
+            items:2,
+            nav:false,
+            loop:true,
+        }
+    }
+})
